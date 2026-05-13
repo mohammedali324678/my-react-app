60d7fdbc67c583148b622b89b2f53136456f6a94
Ali <mohammaedali324678@gmail.com>
2026-05-14 01:44:09 +0530

    add github finder with useEffect and API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import Card from "./card"
+function App()
+{
+  const [user, setUser]=useState(null)
+  const [loading, setLoading]=useState(false)
+  const [username, setUsername]=useState('')
+  const [error, setError]=useState(null)
+  const fetchUser = () => {
+    if(!username) return 
+    setLoading(true)
+    setError(null)
+    fetch(`https://api.github.com/users/${username}`)
+    .then(response=> {
+      if(!response.ok)
+      {
+        throw new Error('user not found')
+      }
+      return response.json()
+    })
+    .then(data=>{
+      setUser(data)
+      setLoading(false)
+    })
+    .catch(err=> {
+      setError(err.message)
+      setLoading(false)
 
-const players = [ 
-                 {id:1, name: "mo salah", position:"rw", nationality:"egypt"},
-                 {id:2, name: "sadio mane", position:"lw", nationality:"Senegal"}, 
-                 {id:3, name: "firmino", position:"cf", nationality:"Brazil"} 
-                ]
-
-function App(){
-  const [count, setcount] = useState(0)
+      console.log(err)
+    })
+  }
   return(
     <>
-    <h1>count: {count}</h1>
-    {count>0 && <h2>count is positive</h2>}
-    {count<0 && <h2>count is negative</h2>}
-    {count==0 && <h2>count is zero</h2>}
-    <button onClick={() =>  setcount(count+1) }> Add</button>
-    <button onClick={() =>  setcount(count-1) }> sub</button>
-    <button onClick={() =>  setcount(0) }> reset</button>
-    
-    <h2>players</h2>
-    {players.map((player) => (
-                                <Card 
-                                key={player.id}
-                                name={player.name}
-                                position={player.position}
-                                nationality={player.nationality}
-                                 />
-    ))}
+    <h1>Github ACCOUNT Finder </h1>
+    <input 
+    type="text" 
+    placeholder="Enter Your Github username" 
+    value={username} 
+    onChange={(e)=>setUsername(e.target.value)}
+    onKeyDown={(e)=> e.key==='Enter' && fetchUser()}
+    />
+
+    <button onClick={fetchUser}>search</button>
+    {error && <p>error:{error}</p> }
+    {loading && <p>still loading......</p>}
+    {user && !loading && (
+      <>
+      <img src={user.avatar_url} alt="file not found"   style={{
+    display: "block",
+    margin: "auto",
+    height: "110px",
+    width: "200px"
+  }
+}/>
+      <p>name:{user.name }</p>
+      <p>bio:{user.bio}</p>
+      <p>followers:{user.followers}</p>
+      <p>public_repository:{user.public_repos}</p>
+      <p>location:{user.location}</p>
+      </>
+    )}
+
+
     </>
   )
+
 }
-export default  App
+export default App;
